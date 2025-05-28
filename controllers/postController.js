a7f3692be3822d33a4ebe934935e41864b998887
simonelupone <simone.lupone@gmail.com>
2025-05-29 01:35:52 +0200

    feat: implement index and show methods in post controller to retrieve posts and their tags

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,10 +1,45 @@
+// recupero variabile di connessione al db
+const connection = require("../data/db");
+
 // metodi controller
 const index = (req, res) => {
-  res.send("Elenco dei post");
+  const sql = "SELECT * FROM posts";
+
+  connection.query(sql, (err, results) => {
+    if (err) return res.status(500).json({ error: "Database query failed" });
+    res.json(results);
+  });
 };
 
 const show = (req, res) => {
-  res.send("Dettaglio post: " + req.params.id);
+  const id = req.params.id;
+
+  const sql = "SELECT * FROM posts WHERE id = ?";
+
+  // query per mostrare anche i tag
+  const tagSql = `
+    SELECT T.*
+    FROM tags T
+    JOIN post_tag PT ON T.id = PT.tag_id
+    WHERE PT.post_id = ?;
+  `;
+
+  connection.query(sql, [id], (err, postResult) => {
+    if (err)
+      return res.status(500).json({ error: "Database query failed" + err });
+
+    const post = postResult[0];
+
+    connection.query(tagSql, [id], (err, tagResult) => {
+      if (err)
+        return res.status(500).json({ error: "Database query failed" + err });
+
+      // creo la proprietà da aggiungere all'oggetto post
+      post.tags = tagResult;
+
+      res.json(post);
+    });
+  });
 };
 
 module.exports = { index, show };
